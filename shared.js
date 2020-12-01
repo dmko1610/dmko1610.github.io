2203b73914de3d628d5a5db792c8a1ad4c20bdab
Dmitry Kovalev <trigger1610@gmail.com>
2020-12-01 15:28:59 +0400

    refactoring js functions added sandwich button to all screens

--- shared.js
+++ shared.js
@@ -2,15 +2,38 @@ let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
 let selectPlanButton = document.querySelectorAll(".plan button");
 let negativeButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
-for (let i = 0; i < selectPlanButton.length; i++) {
-  selectPlanButton[i].addEventListener("click", function () {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
-  });
+function openModal() {
+  modal.classList.add("open");
+  backdrop.classList.add("open");
 }
 
-negativeButton.addEventListener("click", () => {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
-});
+function closeModal() {
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+}
+
+function openSidebar() {
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+}
+
+function closeSidebar() {
+  mobileNav.classList.remove("open");
+  closeModal();
+}
+
+for (let i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", openModal);
+}
+if (negativeButton) {
+  negativeButton.addEventListener("click", closeModal);
+}
+
+backdrop.addEventListener("click", closeSidebar);
+
+toggleButton.addEventListener("click", openSidebar);
