aa73570ddea987956c47021e65c847c9aa005648
dmko1610 <trigger1610@gmail.com>
2022-11-06 02:25:02 +0500

    added timeline header

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,3 +1,17 @@
 export default function Timeline() {
-  return (<></>)
+  return (
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        <h1 className="text-2xl font-bold underline underline-offset-8 decoration-4 text-stone-700 mb-5">
+          Timeline
+        </h1>
+        <ol>
+          
+        </ol>
+        <ol></ol>
+        <ol></ol>
+        <ol></ol>
+      </div>
+    </div>
+  )
 }
