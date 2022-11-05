8aa629f854380a90a19373e725ca75878d1a757a
dmko1610 <trigger1610@gmail.com>
2022-11-06 02:04:13 +0500

    added empty portfolio section added fence icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from './components/Footer'
 import Intro from './components/Intro'
+import Portfolio from './components/Portfolio'
 
 function App() {
   return (
     <div className="bg-stone-300 text-stone-900 min-h-screen font-mono">
       <Intro />
+      <Portfolio />
       {/* <Timeline />
       <Contact /> */}
       <Footer />
---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,14 @@
+export default function Portfolio() {
+  return (
+    <div className="flex justify-center py-10 text-center">
+      <div className="rounded-xl border-stone-500 border-2 w-5/6 h-full border-solid">
+        <div className="flex justify-center">
+          <img className="mt-10" src="./fence.svg" alt="fence" />
+        </div>
+        <p className="text-3xl my-10">
+          Section is under reconstruction! Please scroll down!
+        </p>
+      </div>
+    </div>
+  )
+}
