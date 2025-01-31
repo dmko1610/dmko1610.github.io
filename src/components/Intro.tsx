4c2cd631ba3c00c4aac239cc7fd60f8fd347680a
Dmitry Kovalev <42618412+dmko1610@users.noreply.github.com>
2025-01-31 18:24:42 +0400

    Update Intro.tsx

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -11,11 +11,11 @@ export default function Intro() {
           Software Engineer & JS Developer
         </p>
         <p className="text-sm dark:text-stone-300 max-w-xl mb6 font-bold">
-          Software Engineer with more that 6+ years overall experience in IT
-          and 3+ years of experience as a JS Developer. Using React and React-Native
+          Software Engineer with more that 8+ years overall experience in IT
+          and 5+ years of experience as a JS Developer. Using React and React-Native
           for developing web and mobile applications. Has been awarded with bachelor's degree
           in Programming Engineering and with master's degree in Applied Informatics. Was involved
-          in 3 different projects as a React-Native Developer. Besides React and RN also familiar
+          in 4 different projects as a React-Native Developer. Besides React and RN also familiar
           with NodeJS, Java, OracleSQL. As a version control system prefers Git, but also worked with SVN (Tortoise client).
           Commited to the principles of XP (Extreme programming) likes DRY, KISS and YAGNI principles.
         </p>
